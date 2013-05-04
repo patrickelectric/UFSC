abe2794e9ba14534f65a87353c66d221d4703b75
Patrick JP <patrickelectric@gmail.com>
2013-05-04 12:21:51 -0300

    att

--- algorit/src/main.cpp
+++ algorit/src/main.cpp
@@ -5,8 +5,8 @@ int main ()
 	Registrador8b.add8Bit(entrada);
 	while(1)															// salva os valores da interrupção no registrador
 	{						
-		NewMascara();				
-		NewInterrupt();
+		NewMascara();													// funcao para poder mudar a mascara antes de um ciclo de clock 		
+		NewInterrupt();													// funcao para poder mudar o sinal de interrupcao 
 		sleep(1);
 		controlador(Decodificador8x4(Registrador8b.memoria));         	// roda a controlador
 	}
